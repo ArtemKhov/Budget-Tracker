914944cc576c6792df59f97c43354a1695f26cba
Artem <artem.kotyakhov@gmail.com>
2023-06-16 19:59:11 +0300

    48: rework login system

--- sign_in.py
+++ sign_in.py
@@ -8,17 +8,9 @@ def login_window(parent):
         signin_window.destroy()
         parent.deiconify()
 
-    def user_enter_focus(event):
-        if username_form.get() == "username":
-            username_form.delete(0, END)
-            username_form.configure(foreground="white")
-        elif username_form.get() == '':
-            username_form.insert(0, "username")
-            username_form.configure(foreground="grey")
-
     # creating signin window
     signin_window = tkboot.Toplevel()
-    signin_window.geometry("600x360")
+    signin_window.geometry("600x290")
     signin_window.title('Login')
     # signin_window.protocol('WM_DELETE_WINDOW', parent.quit)
 
@@ -36,27 +28,15 @@ def login_window(parent):
     login_frame.grid(row=0, column=0, padx=50, pady=50, sticky="WE")
 
     # Welcome Label
-    welcome_text = tkboot.Label(login_frame, text="Quick Start", font=("", 20, "bold"), background="#20374C")
+    welcome_text = tkboot.Label(login_frame, text="Budget Tracker", font=("", 20, "bold"), background="#20374C")
     welcome_text.grid(row=0, column=0, pady=(20,5))
 
     # Instruction block
     instruction_text = tkboot.Label(login_frame,
-                                    text="The username will be tied to your budget",
+                                    text="Keep track of your money expenditures",
                                     font=("", 11),
                                     background="#20374C")
     instruction_text.grid(row=1, column=0, pady=(0, 20))
-
-    # Username Block
-    username_text = tkboot.Label(login_frame,
-                                 text="Username",
-                                 font=("", 12))
-    username_text.grid(row=2, column=0, sticky="W", padx=107, pady=(0, 2))
-
-    username_form = tkboot.Entry(login_frame, style="info", width=30, font=("", 12), foreground="grey")
-    username_form.grid(row=3, column=0, pady=(0, 10), ipady=5)
-    username_form.insert(0, 'username')
-    username_form.bind("<FocusIn>", user_enter_focus)
-    username_form.bind("<FocusOut>", user_enter_focus)
 
     # Login Button
     sign_in_button = tkboot.Button(login_frame,
